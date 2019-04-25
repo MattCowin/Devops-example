9e25c9099fd992df6b4d3ff514890562405fac3f
matt <mattcowin82@gmail.com>
2019-04-25 09:24:07 -0400

    fixed the bad pull

--- src/test/java/com/revature/test/TodoDaoTest.java
+++ src/test/java/com/revature/test/TodoDaoTest.java
@@ -9,19 +9,14 @@ import com.revature.dao.TodoDaoImpl;
 
 public class TodoDaoTest {
 
-<<<<<<< HEAD
+
 		private final TodoDao dao = new TodoDaoImpl();
 		
 		@Test
 		public void getAllTodos_ShouldReturn1Record() {
 			assertEquals(1, dao.getAllTodos().size());
 		}
-=======
-	private final TodoDao dao = new TodoDaoImpl();
 
-	@Test
-	public void getAllTodos_ShouldReturn1Record() {
-		assertEquals(1, dao.getAllTodos().size());
-	}
->>>>>>> 9ebfb8f871bfdd941f38cb1e8c251fc4209a6c05
+	
+
 }
